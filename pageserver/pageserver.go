73794f3f9b8cd2da3d610debfc97e8b354da3c30
Monmohan Singh <monmohan.singh@autodesk.com>
2016-05-13 17:36:28 +0800

    default port 12345 for page server

--- pageserver/pageserver.go
+++ pageserver/pageserver.go
@@ -7,11 +7,14 @@ import (
 	"net/http"
 )
 
-var port = flag.Int("port", 10001, "help message for flagname")
+var port = flag.Int("port", 12345, "port to run the server on, default is 12345")
+var setCookie = flag.Bool("set-cookie", false, "enable to set cookie in response")
 
 func fileHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Requested URL %v\n", r.URL.Path)
-	http.SetCookie(w, &http.Cookie{Name: "token", Value: "secret_token"})
+	if *setCookie {
+		http.SetCookie(w, &http.Cookie{Name: "token", Value: "secret_token"})
+	}
 	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
